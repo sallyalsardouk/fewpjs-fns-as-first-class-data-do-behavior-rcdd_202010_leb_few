c3c77fbad02ce8ff9c7354723842fdb05ae682db
sally al sardouk <sally.sardouk98@gmail.com>
2020-11-17 21:11:44 +0000

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(timeString)
+{  
+  let timeString1 = timeString.split(":")
+  let hours= timeString1[0];
+  let min =timeString1[1];
+  let hour1=parseInt(hours);
+  let min1=parseInt(min);
+  
+  if (hour1<12){
+    return"Good Morning";
+  }
+  else if ((hour1>=12) && (hour1<=17 ))
+    { return "Good Afternoon";
+  
+  }
+  else if(hour1>17){
+   return "Good Evening";
+  }
+  
+}
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
